600012bf02dc91cf921b7f3d70b94d4852fc167e
Ruben Robles <d8vjork@outlook.com>
2022-06-23 13:01:18 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# vscode-laravel-pint
-Integrates Laravel Pint into your VSCode projects for automatic code formatting
+# Laravel Pint
+
+**This extension is NOT official from the Laravel team.** [Take a look into the official project](https://github.com/laravel/pint).
+
+Integrates Laravel Pint into your VSCode projects for automatic code formatting.
+
+## Getting started
+
+Just install and enable the extension, remember to configure the option `laravel-pint.executablePath` setting it as an absolute (if you installed it as a composer global dependency) or relative path (to your workspace). **For your convenience, this option defaults to your local workspace path (`vendor/bin/pint`)**.
+
+Then save any file and it will auto-format it for you, or press `Ctrl+Shift+P` on Windows (`Cmd+Shift+P` on Mac OS) and type _"Format document using Laravel Pint"_ this will format the current active file.
+
+## License
+
+**Logo is property of Laravel Team or Laravel Pint project.**
+
+This project is open-sourced software licensed under the [MIT license](LICENSE.md).
---
+++ src/extension.ts
@@ -0,0 +1,79 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { spawn } from 'child_process';
+import { existsSync } from 'fs';
+import path = require('path');
+import { commands, ExtensionContext, OutputChannel, Uri, window, workspace, WorkspaceConfiguration } from 'vscode';
+
+let saveWhenFormatted = false;
+let outputChannel: OutputChannel | null;
+
+function getActiveDocumentPath() {
+  const activeDocumentUri = window.activeTextEditor?.document.uri;
+
+  if (!activeDocumentUri) {
+    return undefined;
+  }
+
+  return activeDocumentUri;
+}
+
+function format(file: Uri, config: WorkspaceConfiguration) {
+  const workspaceRootPath = workspace.workspaceFolders?.[0].uri.fsPath;
+  let executableFullPath = config.get<string>('executablePath');
+
+  if (executableFullPath && !path.isAbsolute(executableFullPath) && workspaceRootPath) {
+    executableFullPath = path.posix.resolve(workspaceRootPath, executableFullPath);
+  }
+  
+  if (!executableFullPath || !existsSync(executableFullPath)) {
+    return window.showErrorMessage('Executable not found for Laravel Pint.');
+  }
+
+  outputChannel?.appendLine(`Formatting file "${workspace.asRelativePath(file.path)}"`);
+  
+  const exec = spawn(executableFullPath, [workspace.asRelativePath(file.path), '--preset', 'laravel'], {
+    cwd: workspaceRootPath
+  });
+}
+
+export function activate(context: ExtensionContext) {
+  outputChannel = window.createOutputChannel("Laravel Pint");
+  const config = workspace.getConfiguration('laravel-pint');
+
+  context.subscriptions.push(commands.registerCommand('laravel-pint.format', () => {
+    if (window.activeTextEditor?.document.languageId !== 'php') {
+      return;
+    }
+
+    const activeEditorFile = getActiveDocumentPath();
+
+    if (activeEditorFile) {
+      format(activeEditorFile, config);
+    }
+  }));
+
+  context.subscriptions.push(workspace.onWillSaveTextDocument((e) => {
+    if (e.document.languageId !== 'php' || saveWhenFormatted === true) {
+      return;
+    }
+    
+    const activeEditorFile = getActiveDocumentPath();
+
+    if (activeEditorFile) {
+      outputChannel?.appendLine(JSON.stringify(activeEditorFile));
+
+      format(activeEditorFile, config);
+    }
+  }));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+  if (outputChannel) {
+    outputChannel.clear();
+    outputChannel.dispose();
+  }
+
+  outputChannel = null;
+}
