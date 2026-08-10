47611c9c9534e8fed3b272292a839de0065730c5
Ruben R <ruben@opensoutheners.com>
2026-08-10 16:52:34 +0200

    fix: ignore node_modules from watched paths (glob)

--- src/util.ts
+++ src/util.ts
@@ -59,6 +59,7 @@ export async function resolvePathFromWorkspaces(pattern: string, relativeTo: Wor
   const matchedPaths = await glob(pattern, {
     cwd: relativeTo.uri.fsPath,
     absolute: true,
+    ignore: ['**/node_modules/**'],
   });
 
   return matchedPaths;
